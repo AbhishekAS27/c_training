da9ad44533460b45ea62a5a9af5a2e0bc259119a
AbhishekAS27 <abhishekasa25@gmail.com>
2025-11-21 14:03:23 +0530

    Add files via upload

---
+++ struct_inp2.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+struct Book
+{
+    char name [100];
+    char author [100];
+    int year;
+};
+void main()
+{
+    int n;
+    printf("enter number of Book :");
+    scanf("%d", &n);
+    struct Book b[n];
+    for(int i = 0; i<n; i++)
+    {
+        printf("enter name of Book:");
+        scanf("%s", b[i].name);
+
+        printf("enter author of BOok:");
+        scanf("%sn", b[i].author);
+
+        printf("enter the year of publication: ");
+        scanf("%d",&b[i].year);
+    }
+    printf("your information:\n");
+    for (int i  = 0; i < n; i++)
+    {
+        printf("%s\n",b[i].name);
+        printf("%s\n", b[i].author);
+        printf("%d\n",b[i].year);
+    }
+
+}
